b981ab0076f66e1149dac3ee048ee8274ccf28dc
Ege YourLastName <hello@egeuysal.com>
2025-05-22 14:29:11 -0500

    Add the basics

---
+++ social-api/backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/egeuysall/social-api
+
+go 1.24.3
---
+++ social-api/frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col h-screen w-screen items-center justify-center">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+        Hello World!
+      </h1>
+    </main>
+  );
+}
